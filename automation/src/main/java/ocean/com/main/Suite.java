e8d2be9e9b3115f153eab9515049fe4048361aac
Mohit Goel <Mohit.Goel@NIIT-Tech.com>
2019-09-16 12:24:56 +0530

    new contract

--- automation/src/main/java/ocean/com/main/Suite.java
+++ automation/src/main/java/ocean/com/main/Suite.java
@@ -50,8 +50,8 @@ public class Suite extends ReadData {
 			System.out.println(device);
 			
 			appCapabilities.setCapability("deviceName", device);
-			String path = System.getenv("LOCALAPPDATA") + "\\OceanDev\\Ocean.exe";
-			//String path = "C:\\Users\\mohit.goel\\AppData\\Local\\OceanDev\\Ocean.exe";
+			//String path = System.getenv("LOCALAPPDATA") + "\\OceanDev\\Ocean.exe";
+			String path = "C:\\Users\\mohit.goel\\AppData\\Local\\OceanDev\\Ocean.exe";
 			System.out.println(path);
 			appCapabilities.setCapability("app", path);
 			// appCapabilities.setCapability("app", "C:\\Windows\\System32\\notepad.exe");
