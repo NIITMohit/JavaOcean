95e8f477bc8bfa923c829ae5ff66af26e7bdc9d0
Mohit Goel <Mohit.Goel@NIIT-Tech.com>
2019-11-13 10:56:08 +0530

    Stable CHeckin

--- automation/src/main/java/ocean/modules/pages/pricingModulePages.java
+++ automation/src/main/java/ocean/modules/pages/pricingModulePages.java
@@ -16,4 +16,71 @@ public class pricingModulePages extends CommonFunctions {
 	public void visitPriceSheetListTab() throws Exception {
 		click("clickPricingSheetListTab");
 	}
+
+	/**
+	 * This function is used delete a price sheet based on search paramater
+	 * 
+	 * @param priceSheet on which priceSheet needs to be searched
+	 */
+	public void deletePriceSheet(String priceSheet) {
+		try {
+			String searchPriceSheet = searchPriceSheet(priceSheet);
+			if (searchPriceSheet.toLowerCase().equals(priceSheet.toLowerCase())) {
+				click("getPriceSheetCode");
+				// right click on price sheet to delete
+				rightClick("getPriceSheetCode");
+				// Click Delete to delete price sheet
+				click("clickDeleteToDeletePS");
+				// Click Ok, to confirm delete ,
+				click("clickOK");
+				click("clickOK");
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+
+	}
+
+	/**
+	 * This common function is used delete a price sheet based on search paramater
+	 * 
+	 * @param priceSheet on which priceSheet needs to be searched
+	 */
+	public void importPriceSheet(String newPriceSheetCode) {
+		try {
+			click("clickImportButton");
+			// Type price sheet name
+			type("typeNameOfNewPS", "Automation Price Sheet");
+			// click browse to upload pricesheet
+			click("clickBrowse");
+			//// price sheet path, taken at run time, price sheet exist in Repository common
+			//// folder
+			String priceSheetPath = currentDir + "\\Repository\\PriceSheetAutomation.xlsx";
+			// price sheet path
+			type("priceSheetUploadPath", priceSheetPath);
+			// click open
+			click("clickOpenbutton");
+			// type price sheet code
+			type("typeCodeOfNewPS", newPriceSheetCode);
+			// CLick ok
+			click("clickOK");
+			//// Post successful upload verify newprice sheet is uploaded
+		} catch (Exception e) {
+			throw e;
+		}
+
+	}
+
+	/**
+	 * This common function is used search a price sheet based on search paramater
+	 * 
+	 * @param priceSheet on which priceSheet needs to be searched
+	 */
+	public String searchPriceSheet(String newPriceSheetCode) {
+		String priceSheet = "";
+		type("typeProgramCode", newPriceSheetCode);
+		// Verify Program code = newProgramCode exists in search results,
+		priceSheet = getValue("getPriceSheetCode");
+		return priceSheet;
+	}
 }
