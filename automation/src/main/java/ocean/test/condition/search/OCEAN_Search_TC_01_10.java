4a3378310149438254f6254d3614d3f71794e71c
Mohit Goel <Mohit.Goel@NIIT-Tech.com>
2019-11-19 12:06:12 +0530

    Search All Code Checkin

--- automation/src/main/java/ocean/test/condition/search/OCEAN_Search_TC_01_10.java
+++ automation/src/main/java/ocean/test/condition/search/OCEAN_Search_TC_01_10.java
@@ -1,29 +1,28 @@
 package ocean.test.condition.search;
 
-import static org.testng.Assert.assertEquals;
-
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
-import org.testng.SkipException;
 import org.testng.annotations.Test;
 
-import ocean.common.CommonFunctions;
 import ocean.common.DataProviderClass;
-import ocean.modules.pages.pricingModulePages;
 import ocean.modules.pages.searchModulePages;
 
 /**
- * OCEAN_Pricing_TC01 class automates Ocean Pricing module Test Condition 01,
- * which holds 1 Test Case; Test Condition Description : Validate price sheet
- * content after their import into OCEAN
+ * OCEAN_Search_TC_01_10 class automates Ocean Search module Test Condition
+ * 01-10, which holds 10 Test Cases; Test Condition Description : Validate
+ * Search sheet content after their import into OCEAN
  * 
  * @author Mohit Goel
  */
-public class OCEAN_Search_TC_01 extends searchModulePages {
-	@Test(priority = 1, groups = "regression", dataProvider = "searchContract", dataProviderClass = DataProviderClass.class)
-	public void searchContractWithAnyInputFielsd(String[] inputArray) throws Exception {
+public class OCEAN_Search_TC_01_10 extends searchModulePages {
+	/**
+	 * This function covers all search cases based on search parameters given from
+	 * excel
+	 * 
+	 */
+	@Test(priority = 1, groups = "regression", dataProvider = "searchContract", dataProviderClass = DataProviderClass.class, description = "Search test case,Contract should be Search from the given search sheet under Search Section.")
+	public void searchContractWithAnyInputField(String[] inputArray) throws Exception {
 		//// create data to fill required values in search window
 		HashMap<String, String> uiSearchData = null;
 		if (Arrays.stream(inputArray).anyMatch("*"::equals)) {
