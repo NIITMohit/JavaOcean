5c82b43f783efde725054766b5daffff22875426
Mohit Goel <Mohit.Goel@NIIT-Tech.com>
2019-12-30 14:16:26 +0530

    Stablee

--- automation/src/main/java/ocean/com/main/Suite.java
+++ automation/src/main/java/ocean/com/main/Suite.java
@@ -76,7 +76,7 @@ public class Suite extends ReadData {
 			appCapabilities.setCapability("app", oceanApplicationPath);
 			// appCapabilities.setCapability("app", oceanApplicationPath);
 			//// win app driver session attached successfully
-			//windowsDriver = new WindowsDriver(new URL("http://127.0.0.1:4723/wd/hub"), appCapabilities);
+			windowsDriver = new WindowsDriver(new URL("http://127.0.0.1:4723/wd/hub"), appCapabilities);
 			//// create report folder in running directory
 			createReportFolder();
 			//// Create reports
